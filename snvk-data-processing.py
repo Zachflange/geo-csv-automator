55b0046133cfda00631670af54e88e453b30869e
Zachflange <128617291+Zachflange@users.noreply.github.com>
2026-04-14 14:03:44 +0200

    Update snvk-data-processing.py

--- snvk-data-processing.py
+++ snvk-data-processing.py
@@ -5,23 +5,31 @@ filename = "snvk-tool-data_epsg25832.csv"
 fields = ['id', 'name', 'kategorie', 'nutzung', 'räume_funktionen', 'nutzungsfläche', 'lhm_orga', 'x', 'y', 'bezirk', 'hausnummer', 'gebäude', 'flurstück', 'träger', 'eigentumsverhältnis', 'objektstatus']
 rows = [
             ["test1", "test2", "test3"],
-            ["test4", "test5"]
+            ["test4", "test5", "objektorientiert"]
         ]
 
-# Get the user's home directory
-home = os.path.expanduser("~")
+def set_directory():
+    # Get the user's home directory
+    home = os.path.expanduser("~")
 
-# Set the desktop path based on the operating system
-if os.name == 'nt':  # Windows
-    desktop = os.path.join(home, 'Desktop')
-elif os.name == 'posix':  # macOS or Linux
-    desktop = os.path.join(home, 'Desktop')
+    # Set the desktop path based on the operating system
+    if os.name == 'nt':  # Windows
+        desktop = os.path.join(home, 'Desktop')
+    elif os.name == 'posix':  # macOS or Linux
+        desktop = os.path.join(home, 'Desktop')
+    
+    # Change the current working directory to the desktop
+    os.chdir(desktop)
+    print(f"Current working directory is now: {os.getcwd()}")
 
-# Change the current working directory to the desktop
-os.chdir(desktop)
+def open_file(filepath, filename):
+    print(f"file " + filename + " opened")
 
-print(f"Current working directory is now: {os.getcwd()}")
+def extract_data(filepath, ):
+    print(f"data extracted from" + filename)
+    return rows
 
+'''
 file_path = os.getcwd()+filename
 print(file_path)
 
@@ -31,9 +39,17 @@ try:
         file.write("Hello, World!")
 except FileExistsError:
     print(f"The file '{file_path}' already exists.")
+'''
 
-with open(filename, 'w', newline='', encoding='utf-8') as csvfile:       
-    writer = csv.writer(csvfile, delimiter=';') # Create writer object
-    writer.writerow(fields)             # Write header
-    writer.writerows(rows)              # Write multiple rows
-    print('data is written in ' + filename)
+def write_export_file(fields, rows):
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:       
+        writer = csv.writer(csvfile, delimiter=';') # Create writer object
+        writer.writerow(fields)             # Write header
+        writer.writerows(rows)              # Write multiple rows
+        print('data is written in ' + filename)
+
+# Run
+if __name__ == "__main__":
+    print(f"-------------------------------------------------------------------------------------/nsnvk-data-prozessing.py is running")
+    set_directory()
+    write_export_file(fields, rows)
